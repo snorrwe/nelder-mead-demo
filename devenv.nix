e2aca92adeb23536d026687d4d0925552afe5067
Daniel Kiss <littlesnorrboy@gmail.com>
2024-12-05 17:35:28 +0100

    init

---
+++ devenv.nix
@@ -0,0 +1,44 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ git ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    uv = {
+      enable = true;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
